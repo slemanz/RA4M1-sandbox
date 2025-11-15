d289ed87de6cb2213a4fa51c35ee20f941fa0674
slemanz <william_sleman@hotmail.com>
2025-11-15 15:46:39 -0300

    app(feat): clock driver

--- app/Src/main.c
+++ app/Src/main.c
@@ -1,20 +1,22 @@
 #include "ra4m1.h"
 #include "config.h"
 
+#include "driver_clock.h"
 #include "driver_gpio.h"
 #include "driver_systick.h"
 
 
 int main(void)
 {
+    clock_init();
     config_drivers();
-    systick_init(500000, 1000);
+    systick_init(clock_system_get(), 1000);
 
     uint64_t start_time = ticks_get();
 
     while(1)
     {
-        if((ticks_get() - start_time) >= 500)
+        if((ticks_get() - start_time) >= 100)
         {
             GPIO_ToggleOutputPin(PORT1, GPIO_PIN_NO_11);
             start_time = ticks_get();
