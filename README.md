b2870b00366e47ae4aa0f1a4507885b69c3deb6b
slemanz <william_sleman@hotmail.com>
2025-12-30 18:34:17 -0300

    app(feat): retarget printf

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Arduino R4 Minima Pinout
 
 | Arduino | MCU |
 | --- | --- |
-| Pin 13 | P111_GPT3_A |
-| Pin 8  | P304_GPT7_A |
+| Pin 13 | P111_GPT3_A   |
+| Pin 8  | P304_GPT7_A   |
+| Pin 1  | P302_SCI2_TXD |
--- app/Src/main.c
+++ app/Src/main.c
@@ -1,18 +1,35 @@
+#include <stdio.h>
 #include "config.h"
 
 #include "bsp/led.h"
 #include "core/simple-timer.h"
 
+#include "driver_sci.h"
+
 
 int main(void)
 {
     config_drivers();
+    sci2_init();
 
     simple_timer_t timer_blinky;
+    simple_timer_t timer_ch;
     simple_timer_setup(&timer_blinky, 500, true);
+    simple_timer_setup(&timer_ch, 1000, true);
+
+    printf("Init the board!\r\n");
 
     while(1)
     {
         if(simple_timer_has_elapsed(&timer_blinky)) led_toggle();
+
+        //if(simple_timer_has_elapsed(&timer_ch)) sci2_write_byte('A');
     }
+}
+
+// printf retarget
+extern int __io_putchar(int ch)
+{
+    sci2_write_byte((uint8_t)ch);
+    return ch;
 }
