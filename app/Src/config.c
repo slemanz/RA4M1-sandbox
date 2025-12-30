d198bebc5d1e4a5bf12ccf2a5cdfdecacec816f1
slemanz <william_sleman@hotmail.com>
2025-12-30 11:00:51 -0300

    app(feat): add simple-timer api to blinky

--- app/Src/config.c
+++ app/Src/config.c
@@ -3,7 +3,9 @@
 /************************************************************
 *                       DRIVERS                             *
 *************************************************************/
+#include "driver_clock.h"
 #include "driver_gpio.h"
+#include "driver_systick.h"
 
 static const GPIO_PinConfig_t GPIO_ConfigTable[] = {
     {PORT1, GPIO_PIN_NO_11, GPIO_MODE_OUT, GPIO_PIN_DRIVE_LOW, GPIO_PIN_OP_TYPE_CMOS, GPIO_PIN_PU_DISABLE, GPIO_PIN_PSEL_DEFAULT},
@@ -12,5 +14,7 @@ static const GPIO_PinConfig_t GPIO_ConfigTable[] = {
 
 void config_drivers(void)
 {
+    clock_init();
     GPIO_Init_table(GPIO_ConfigTable, sizeof(GPIO_ConfigTable)/sizeof(GPIO_ConfigTable[0]));
+    systick_init(clock_system_get(), 1000);
 }
--- app/Src/main.c
+++ app/Src/main.c
@@ -1,28 +1,18 @@
-#include "ra4m1.h"
 #include "config.h"
 
-#include "driver_clock.h"
-#include "driver_gpio.h"
-#include "driver_systick.h"
-
 #include "bsp/led.h"
+#include "core/simple-timer.h"
 
 
 int main(void)
 {
-    clock_init();
     config_drivers();
-    systick_init(clock_system_get(), 1000);
 
-    uint64_t start_time = ticks_get();
+    simple_timer_t timer_blinky;
+    simple_timer_setup(&timer_blinky, 500, true);
 
     while(1)
     {
-        if((ticks_get() - start_time) >= 100)
-        {
-            GPIO_ToggleOutputPin(PORT1, GPIO_PIN_NO_11);
-            led_toggle();
-            start_time = ticks_get();
-        }
+        if(simple_timer_has_elapsed(&timer_blinky)) led_toggle();
     }
 }
