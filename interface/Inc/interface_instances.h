7b4e5ce41945a08793d6ddc134f15b7dc2841e5b
slemanz <william_sleman@hotmail.com>
2026-01-03 09:10:59 -0300

    interface(feat): add instances file

---
+++ interface/Inc/interface_instances.h
@@ -0,0 +1,12 @@
+#ifndef INC_INTERFACE_INSTANCES_H_
+#define INC_INTERFACE_INSTANCES_H_
+
+/************************************************************
+*                   COMM INSTANCES                          *
+*************************************************************/
+
+#include "interface_comm.h"
+
+extern const Comm_Interface_t sci2_protocol;
+
+#endif /* INC_INTERFACE_INSTANCES_H_ */
