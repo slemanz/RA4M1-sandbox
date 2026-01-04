18328ddde32ea470c87c877c333f258922954ae5
slemanz <william_sleman@hotmail.com>
2026-01-03 21:08:48 -0300

    driver(feat): add better sci init function

--- drivers/Src/driver_sci.c
+++ drivers/Src/driver_sci.c
@@ -9,24 +9,71 @@ static uint8_t calculate_brr(uint32_t baud, uint32_t clock)
     return (uint8_t)calc;
 }
 
-void sci2_init(void)
+void SCI_StopControl(SCI_RegDef_t *pSCIx, uint8_t EnorDi)
 {
-    // clear stop mode SCI2
-    MSTPCRB &= ~(1 << 29);
-
-    // set baudrate
-    SCI2->BRR = calculate_brr(115200, clock_system_get());
-    for(uint32_t i = 0; i < 100; i++);
-
-    // transmit enable
-    SCI2->SCR |= (1 << 5);
-
-    // receive enable
-    SCI2->SCR |= (1 << 4);
-
-    // interrupt rxi enable
-    SCI2->SCR |= (1 << 6);
+    if(EnorDi == ENABLE)
+    {
+        if(pSCIx == SCI2)
+        {
+            SCI2_STOP_EN();
+        }
+    }else
+    {
+        if(pSCIx == SCI2)
+        {
+            SCI2_STOP_DI();
+        }
+    }
 }
+
+void SCI_BaudConfig(SCI_RegDef_t *pSCIx, uint32_t Baud)
+{
+    pSCIx->BRR = calculate_brr(Baud, clock_system_get());
+    for(uint32_t i = 0; i < 100; i++);
+}
+
+void SCI_Init(SCI_Config_t *pSCIConfig)
+{
+    SCI_StopControl(pSCIConfig->pSCIx, DISABLE);
+    SCI_BaudConfig(pSCIConfig->pSCIx, pSCIConfig->SCI_Baud);
+
+    uint8_t temp = 0;
+    if(pSCIConfig->SCI_NoOfStopBits == SCI_STOPBITS_2) temp |= (SCI_SMR_STOP_MSK);
+
+    if(pSCIConfig->SCI_ParityControl == SCI_PARITY_EN_EVEN)
+    {
+        temp |= (SCI_SMR_PE_MSK);
+    }else if(pSCIConfig->SCI_ParityControl == SCI_PARITY_EN_ODD)
+    {
+        temp |= (SCI_SMR_PE_MSK | SCI_SMR_PM_MSK);
+    }
+    pSCIConfig->pSCIx->SMR = temp;
+
+    temp = 0;
+    if(pSCIConfig->SCI_Mode == SCI_MODE_ONLY_TX)
+    {
+        temp |= SCI_SCR_RE_MSK;
+    }else if(pSCIConfig->SCI_Mode == SCI_MODE_ONLY_RX)
+    {
+        temp |= SCI_SCR_TE_MSK;
+    }else if(pSCIConfig->SCI_Mode == SCI_MODE_TXRX)
+    {
+        temp |= (SCI_SCR_RE_MSK | SCI_SCR_TE_MSK);
+    }
+    pSCIConfig->pSCIx->SCR = temp;
+}
+
+void SCI_InterruptConfig(SCI_RegDef_t *pSCIx, uint8_t regs, uint8_t EnorDi)
+{
+    if(EnorDi == ENABLE)
+    {
+        pSCIx->SCR |= regs;
+    }else if(EnorDi == DISABLE)
+    {
+        pSCIx->SCR &= ~(regs);
+    }
+}
+
 
 void sci2_write_byte(uint8_t ch)
 {
