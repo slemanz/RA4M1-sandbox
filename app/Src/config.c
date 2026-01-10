fa7351383f1be5840cf5eb8c8135004387d684fc
slemanz <william_sleman@hotmail.com>
2026-01-10 15:15:17 -0300

    app(refact): improve configs functions

--- app/Src/config.c
+++ app/Src/config.c
@@ -5,16 +5,56 @@
 *************************************************************/
 #include "driver_clock.h"
 #include "driver_gpio.h"
-#include "driver_systick.h"
 
 static const GPIO_PinConfig_t GPIO_ConfigTable[] = {
     {PORT1, GPIO_PIN_NO_11, GPIO_MODE_OUT,  GPIO_PIN_DRIVE_LOW, GPIO_PIN_OP_TYPE_CMOS, GPIO_PIN_PU_DISABLE, GPIO_PIN_PSEL_DEFAULT},
     {PORT3, GPIO_PIN_NO_4,  GPIO_MODE_OUT,  GPIO_PIN_DRIVE_LOW, GPIO_PIN_OP_TYPE_CMOS, GPIO_PIN_PU_DISABLE, GPIO_PIN_PSEL_DEFAULT},
 };
 
-void config_drivers(void)
+/************************************************************
+*                      INTERFACE                            *
+*************************************************************/
+
+#include "interface_comm.h"
+
+Comm_Interface_t *serial = NULL;
+
+void config_interface(void)
+{
+    serial = Comm_ProtocolGet(PROTOCOL_SCI2);
+}
+
+// printf retarget
+extern int __io_putchar(int ch)
+{
+    if(serial != NULL) serial->send((uint8_t*)&ch, 1);
+    return ch;
+}
+
+/************************************************************
+*                         CORE                              *
+*************************************************************/
+#include "core/cli.h"
+#include "core/simple-timer.h"
+
+const command_t commands_table[] = {
+    {"help", cli_help, "List all commands."},
+};
+
+void config_core(void)
+{
+    cli_setup(Comm_ProtocolGet(PROTOCOL_SCI2), (command_t*)commands_table, 1);
+}
+
+/************************************************************
+*                         APP                               *
+*************************************************************/
+
+void config_app(void)
 {
     clock_init();
     GPIO_Init_table(GPIO_ConfigTable, sizeof(GPIO_ConfigTable)/sizeof(GPIO_ConfigTable[0]));
-    systick_init(clock_system_get(), 1000);
+
+    config_interface();
+    config_core();
 }
