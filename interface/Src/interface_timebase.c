e46cff05c1ed0952ba0533a0d742c957c46ae8dc
slemanz <william_sleman@hotmail.com>
2026-01-10 10:40:31 -0300

    interface(feat): add timebase implementation

---
+++ interface/Src/interface_timebase.c
@@ -0,0 +1,19 @@
+#include "interface_timebase.h"
+#include "driver_systick.h"
+#include "driver_clock.h"
+
+uint8_t timebase_is_init = 0;
+
+const static timebase_interface_t timer_systick = {
+    .get = ticks_get,
+    .deinit = NULL
+};
+
+
+
+timebase_interface_t *timebase_get(void)
+{
+    if(timebase_is_init == 0) systick_init(clock_system_get(), 1000);
+
+    return (timebase_interface_t*)&timer_systick;
+}
