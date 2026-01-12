feed3305f4897ea2f5b5ae0fe49ac8f6ed27f95a
slemanz <william_sleman@hotmail.com>
2026-01-12 04:42:56 -0300

    interface(refact): change pin names variables

--- interface/Inc/interface_instances.h
+++ interface/Inc/interface_instances.h
@@ -16,8 +16,8 @@ extern const Comm_Interface_t sci2_protocol;
 
 #include "interface_io.h"
 
-extern const IO_Interface_t io0;
-extern const IO_Interface_t io1;
-extern const IO_Interface_t io2;
+extern const IO_Interface_t io0_pin;
+extern const IO_Interface_t io1_pin;
+extern const IO_Interface_t io2_pin;
 
 #endif /* INC_INTERFACE_INSTANCES_H_ */
